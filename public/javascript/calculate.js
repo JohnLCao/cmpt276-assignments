182e49b81c56cef4a2a347cf95ca62ef3da4d79e
John L. Cao <john_caohaha@hotmail.com>
2017-09-20 20:08:32 -0700

    I think thing is basically working. tinker with the styles and make histogram look more cool

--- public/javascript/calculate.js
+++ public/javascript/calculate.js
@@ -2,13 +2,15 @@ var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22
 49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
 
 const bounds = ["max", "ap", "a", "am", "bp", "b", "bm", "cp", "c", "cm", "d", "f"];
-// const letter_grades = ["ap", "a", "am", "bp", "b", "bm", "cp", "c", "cm", "d", "f"];
+const letter_grades = ["ap", "a", "am", "bp", "b", "bm", "cp", "c", "cm", "d", "f"];
 
 var boundMap = {}; //maps a letter grade to its lower bound
 var gradesMap = {}; //maps a letter grade to the number of students with that grade
 
 function calculate(){
+	console.log("I got called!");
 	getBoundValues();
+	gradesMap = {}; //reset gradesMap
 	grades.forEach(function(g){
 		placeGrade(g);
 	});
@@ -17,8 +19,9 @@ function calculate(){
 
 function getBoundValues(){
 	bounds.forEach(function(b){
-		boundMap[b] = Number(document.getElementsByName(b)[0].attributes.value.nodeValue);
+		boundMap[b] = Number(document.getElementsByName(b)[0].value);
 	});
+	console.log(boundMap);
 }
 
 function placeGrade(grade){
@@ -33,11 +36,10 @@ function placeGrade(grade){
 
 function updateHistogram(){
 	console.log(gradesMap);
-	for (var grade in gradesMap){
-		let hist_size = gradesMap[grade];
+	letter_grades.forEach(function(grade){
+		let hist_size = (gradesMap[grade]) ? gradesMap[grade] : 0;
 		document.getElementById(grade).innerHTML = new Array(hist_size + 1).join('O');
-	}
-
+	});
 }
 
 document.addEventListener("DOMContentLoaded", calculate);
