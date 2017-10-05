7cf6bdb41cba9d992df3a3d8750e5fe3575df69c
John L. Cao <john_caohaha@hotmail.com>
2017-10-05 15:42:35 -0700

    add enrolls scaffold controller, views; add seeds file

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,3 @@
 class Course < ActiveRecord::Base
-	has_many :enrolls
+	has_many :enrolls, dependent: :destroy
 end
--- app/models/enroll.rb
+++ app/models/enroll.rb
@@ -1,4 +1,6 @@
 class Enroll < ActiveRecord::Base
 	belongs_to :student
 	belongs_to :course
+	validates :student, presence: true
+	validates :course, presence: true
 end
